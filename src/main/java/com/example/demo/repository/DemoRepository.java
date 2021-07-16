efeea1d08b3c96484f0ae6b2c55bad4841cf41bf
jorgeandrre <jorgeandre-2011@live.com>
2021-07-16 19:43:42 -0300

    Repository get tipo

--- src/main/java/com/example/demo/repository/DemoRepository.java
+++ src/main/java/com/example/demo/repository/DemoRepository.java
@@ -6,4 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DemoRepository extends JpaRepository<Demo, Long> { }
+public interface DemoRepository extends JpaRepository<Demo, Long> { 
+    String SQL_BASE = "SELECT A FROM Demo A WHERE 1=1";
+    String SQL_TIPO_POKEMON = SQL_BASE + " AND A.tipo = ?1";
+
+    @Query(value = SQL_TIPO_POKEMON)
+    List<Demo> findPokemonsTipo(String tipo);
+}
